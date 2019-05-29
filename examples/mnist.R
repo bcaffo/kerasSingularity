c8230710f06686dfe20b789fff0c1bd045b1196d
Brian Caffo <bcaffo@users.noreply.github.com>
2019-05-29 19:56:05 -0400

    Update mnist.R

--- examples/mnist.R
+++ examples/mnist.R
@@ -1,36 +1,43 @@
 ## From the R keras website
 library(keras)
-mnist <- dataset <- mnist()
-x <- train <- mnist$train$x
-y <- train <- mnist$train$y
-x <- test <- mnist$test$x
-y <- test <- mnist$test$y
+mnist <- dataset_mnist()
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+x_test <- mnist$test$x
+y_test <- mnist$test$y
+
 # reshape
-x <- train <- array <- reshape(x <- train, c(nrow(x <- train), 784))
-x <- test <- array <- reshape(x <- test, c(nrow(x <- test), 784))
+x_train <- array_reshape(x_train, c(nrow(x_train), 784))
+x_test <- array_reshape(x_test, c(nrow(x_test), 784))
 # rescale
-x <- train <- x <- train / 255
-x <- test <- x <- test / 255
+x_train <- x_train / 255
+x_test <- x_test / 255
 
-y <- train <- to <- categorical(y <- train, 10)
-y <- test <- to <- categorical(y <- test, 10)
+y_train <- to_categorical(y_train, 10)
+y_test <- to_categorical(y_test, 10)
 
-model <- keras <- model <- sequential()
-model %>%
-      layer <- dense(units = 256, activation = 'relu', input <- shape = c(784)) %>%
-      layer <- dropout(rate = 0.4) %>%
-      layer <- dense(units = 128, activation = 'relu') %>%
-      layer <- dropout(rate = 0.3) %>%
-      layer <- dense(units = 10, activation = 'softmax')
-
-model %>% compile(
+model <- keras_model_sequential() 
+model %>% 
+  layer_dense(units = 256, activation = 'relu', input_shape = c(784)) %>% 
+  layer_dropout(rate = 0.4) %>% 
+  layer_dense(units = 128, activation = 'relu') %>%
+  layer_dropout(rate = 0.3) %>%
+  layer_dense(units = 10, activation = 'softmax')model %>% compile(
       loss = 'categorical_crossentropy',
       optimizer = optimizer <- rmsprop(),
       metrics = c('accuracy')
     )
 
+model %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = optimizer_rmsprop(),
+  metrics = c('accuracy')
+)
+
 history <- model %>% fit(
-      x <- train, y <- train,
-      epochs = 30, batch <- size = 128,
-      validation <- split = 0.2
-    )
+  x_train, y_train, 
+  epochs = 30, batch_size = 128, 
+  validation_split = 0.2
+)
+
+save.image("out.rda")
